b6b5eaa7bcd77457cd1e00b21ae03990475ef24d
Christian Solis <Christian.Solis@fivesixtwo.com>
2021-09-27 13:48:27 -0700

    inital label

---
+++ src/UnityLabel.svelte
@@ -0,0 +1,10 @@
+<svelte:options tag="unity-label" />
+
+<script lang="ts">
+    // cant use dashed props at the moment
+    export let text = `default`;
+    export let forvalue = `default`;
+    export let isDisabled = false;
+</script>
+
+<label for={forvalue} disabled={isDisabled}>{text}</label>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import UnityLabel from './UnityLabel.svelte';
+
+const unityLabel = new UnityLabel(null);
+
+export default unityLabel;
